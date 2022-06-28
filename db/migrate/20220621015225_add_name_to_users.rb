0378db22b7529306190ef0d6b0aaa3fe02beb553
ushioda <yusuke_ushioda@mediado.jp>
2022-06-28 17:40:20 +0900

    add

--- db/migrate/20220621015225_add_name_to_users.rb
+++ db/migrate/20220621015225_add_name_to_users.rb
@@ -2,6 +2,5 @@ class AddNameToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :name, :string
     add_column :users, :role, :integer, default: 0, null: false
-    add_reference :users, :company, foreign_key: true
   end
 end
