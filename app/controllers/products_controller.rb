32894f1f930de7e8b8a0d8844d7cef802cebfe35
ushioda <yusuke_ushioda@mediado.jp>
2022-07-14 06:10:03 +0900

    add

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,11 @@ class ProductsController < ApplicationController
 
   # GET /products or /products.json
   def index
-    @products = Product.where(user_id: current_user.id, discarded_at: nil)
+    @q = Product.ransack(params[:q])
+    @q.sorts = 'updated_at desc' if @q.sorts.empty?
+    @products = @q.result.where(user_id: current_user.id, discarded_at: nil)
+
+    
   end
 
   
--- app/models/product.rb
+++ app/models/product.rb
@@ -16,20 +16,20 @@ class Product < ApplicationRecord
     def self.csv_format_check(file)
       errors = []
     CSV.foreach(file.path, headers: true).with_index(2) do |row, index|
+      product = new(name: row[0], stock: row["在庫数"], price: row["税込価格"], description: row["商品説明"])
       if row[0].present?
-          product = new(name: row[0], stock: row["在庫数"], price: row["税込価格"], description: row["商品説明"])
         errors << "#{index}行目 商品名が空白です" if product.name.blank?
         errors << "#{index}行目 税込価格が空白です" if product.price.blank?
-        errors << "#{index}行目 在庫数が空白です" if product.stock.blank?
-
-
       else
-        product = new(name: row[0], stock: row["在庫数"], price: row["税込価格"], description: row["商品説明"])
         errors << "#{index}行目 商品名が空白です" if product.name.blank?
-
-
-
       end
+      if row["在庫数"].present?
+        errors << "#{index}行目 在庫数が無効です" if product.stock > 999
+      else
+        errors << "#{index}行目 在庫数が空白です" if product.stock.blank?
+      end
+      
+
     end
     errors
   end
@@ -40,22 +40,32 @@ class Product < ApplicationRecord
         new_count = 0
         update_count = 0
         nochange_count = 0
-        CSV.foreach(file.path, headers: true) do |row|
+        failure = 0
+        failures = []
+        CSV.foreach(file.path, headers: true).with_index(2) do |row, index|
           if product = find_by(name: row[0], user_id: current, discarded_at: nil)
             product.assign_attributes(name: row[0], stock: row["在庫数"], price: row["税込価格"], description: row["商品説明"])
             if product.changed?
-              product.save!
-              update_count += 1
+              if product.save
+                update_count += 1
+              else
+                failure += 1
+                failures << "#{index}行目"
+              end
             else
               nochange_count += 1
             end
           else
             product = new(name: row[0], stock: row["在庫数"], price: row["税込価格"], description: row["商品説明"], user_id: current)
-            product.save!
-            new_count += 1
+              if product.save
+              new_count += 1
+              else
+              failure += 1
+              failures << "#{index}行目"
+              end
           end
         end
-        "新規登録：#{new_count}件、更新：#{update_count}件、無変更：#{nochange_count}件"
+        "新規登録：#{new_count}件、更新：#{update_count}件、変更なし：#{nochange_count}件、更新失敗：#{failure}件#{failures}"
       end
       
       def self.updatable_attributes
